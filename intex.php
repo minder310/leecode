20407bf94b634b3e588057977636e49352cb8356
pierre.K <minder309@gmail.com>
2023-12-14 21:06:04 +0800

    力扣每日解題第一天

---
+++ intex.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>力扣解題紀錄</title>
+</head>
+<body>
+    <a href="index1.php">第一題</a><br>
+    <a href="index2.php">第二題</a><br>
+    <a href="index3.php">第三題</a><br>
+    <a href="index4.php">第四題</a><br>
+    <a href="index5.php">第五題</a><br>
+    <a href="index6.php">第六題</a><br>
+    <a href="index7.php">第七題</a><br>
+    <a href="index8.php">第八題</a><br>
+</body>
+</html>
