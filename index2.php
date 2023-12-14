b2b88eae902cd0a3a3c1e692d650d8d26138e9b7
pierre.K <minder309@gmail.com>
2023-12-14 22:43:05 +0800

    第一題做調整，檔名做調整。

---
+++ index2.php
@@ -0,0 +1,14 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+    function isPalindrome($x) {
+        $x=(string)$x;
+        print_r($x);
+    }
+}
+$a = new Solution;
+$a->isPalindrome(121);
