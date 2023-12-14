f94da98ba84d50e79af279ead913ebeec73209a7
pierre.K <minder309@gmail.com>
2023-12-14 23:44:47 +0800

    增加標題

--- index1.php
+++ index1.php
@@ -1,3 +1,4 @@
+<!-- 1. 两数之和 -->
 <?php
 class Solution
 {
@@ -14,7 +15,6 @@ class Solution
         for ($i = 0; $i < $HowLongToArray; $i++) 
         {
             $a =array_keys($nums,$target-$nums[$i]);
-            print_r($a);
             if(isset($a[0]) && $a[0] != $i)
             {
                 return [$i,$a[0]];
@@ -22,5 +22,12 @@ class Solution
         }
     }
 }
-$a = new Solution();
-print_r( $a->twoSum([2, 7, 11, 15], 9));
+/**
+ * 1.先取得陣列長度
+ * 2.使用for迴圈來跑陣列
+ * 3.使用array_keys()函式來取得陣列中指定值的索引
+ * 4.使用isset()函式來判斷陣列中是否有指定值
+ * 5.使用return來回傳陣列
+ * 整體來說是使用單迴圈，然後家array_keys()來確定陣列中是否有指定值。
+ */
+
