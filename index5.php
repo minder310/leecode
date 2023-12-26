81e9c0199116cf92094f85a97ddea2db76b87448
pierre.K <minder309@gmail.com>
2023-12-26 12:58:50 +0800

    第五題解不出來刊別人怎麼解中。

--- index5.php
+++ index5.php
@@ -28,6 +28,7 @@
  */
 // 解題思路
 // 1.先確認括號的數量是否為偶數，如果不是偶數一定錯。
+// 2.如果再{[(為一邊，就要確認另一邊是否有對應的括號。
 // strleng()可以計算字串長度。
 class Solution
 {
@@ -38,30 +39,86 @@ class Solution
      */
     function isValid($s)
     {
-        if (strlen($s) % 2 != 0) {
+        // 判別數值宣告。
+
+        // 第一步先確人是不是偶數。
+        if (strlen($s) % 2 != 0||strlen($s)==0) {
             return false;
         }
+        // 以下是別人解題絲路。我想不出來了。XD
+        $map = [
+            ')' => '(',
+            ']' => '[',
+            '}' => '{',
+        ];
+        $stack = [];
+        for ($i = 0; $i < strlen($s); $i++) {
+            if (in_array($s[$i], ['(', '[', '{'])) {
+                array_push($stack, $s[$i]);
+            } else {
+                if (array_pop($stack) != $map[$s[$i]]) {
+                    return false;
+                }
+            }
+        }
+        return count($stack) == 0;
+
+
+
+        // 第二次解題思路失敗XD
+        // 最大的問題是array_search()，的false==數字0，而陣列中也會有0，所以會出現問題。第一個文字都不會被檢查到。
+        // 第二步將字串拆除成陣列，並宣告為sarray。
+        // $sarray = str_split($s);
+        // // 第三部用來檢驗是否有對應符號，或是錯誤符號在其中。
+        // $ta = array_search("(", $sarray);
+        // echo $ta;
+        // if ($ta != false) {
+        //     echo "第一層判斷";
+        //     if ($sarray[$ta + 1] == "]" || $sarray[$ta + 1] == "}") {
+        //         echo "第二層判斷";
+        //         return false;
+        //     }
+        // }
+        // $tb = array_search("[", $sarray);
+        // echo $tb;
+        // if($tb!=false){
+        //     echo "第三層判斷";
+        //     if ($sarray[$tb + 1] == ")" || $sarray[$tb + 1] == "}") {
+        //         echo "第四層判斷";
+        //         return false;
+        //     }
+        // }
+        // $tc = array_search("{", $sarray);
+        // if($tc!=false){
+        //     echo "第五層判斷";
+        //     if ($sarray[$tc + 1] == "]" || $sarray[$tc + 1] == ")") {
+        //         echo "第六層判斷";
+        //         return false;
+        //     }
+        // }
+        // return true;
+        // 第一次解題絲路。不正確版本不知如何改進，所以重寫。
         // 辨別括號位置是否正確。
         // 只要位置1,3,5,7，或是2,4,6,8
-            $s = str_replace("(","", $s);
-            $s = str_replace(")","",$s);
-            if (strlen($s) % 2 != 0) {
-                return false;
-            }
-            $s = str_replace("[","", $s);
-            $s = str_replace("]","",$s);
-            if (strlen($s) % 2 != 0) {
-                return false;
-            }
-            $s = str_replace("{","", $s);
-            $s = str_replace("}","",$s);
-            if (strlen($s) % 2 != 0) {
-                return false;
-            }
-        if ($s == "") {
-            return true;
-        }
+        //     $s = str_replace("(","", $s);
+        //     $s = str_replace(")","",$s);
+        //     if (strlen($s) % 2 != 0) {
+        //         return false;
+        //     }
+        //     $s = str_replace("[","", $s);
+        //     $s = str_replace("]","",$s);
+        //     if (strlen($s) % 2 != 0) {
+        //         return false;
+        //     }
+        //     $s = str_replace("{","", $s);
+        //     $s = str_replace("}","",$s);
+        //     if (strlen($s) % 2 != 0) {
+        //         return false;
+        //     }
+        // if ($s == "") {
+        //     return true;
+        // }
     }
 }
-$a= new Solution;
-echo $a->isValid("()"); 
+$a = new Solution;
+echo $a->isValid("()");
