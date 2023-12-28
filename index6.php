6c1535f9e65f463ad26d7fd5105808d83507b24e
Pierre.K <minder309@gmail.com>
2023-12-28 14:17:10 +0800

    第六題，ListNode，輸出格式問題。必須解決。

--- index6.php
+++ index6.php
@@ -12,7 +12,8 @@
 
 // 输入：l1 = [], l2 = [0]
 // 输出：[0]
-class Solution {
+class Solution
+{
 
     /**
      * @param ListNode $list1
@@ -20,37 +21,47 @@ class Solution {
      * @return ListNode
      * 簡單來說就是將兩個陣列合併，並且排序。
      */
-    function preall($alllist){
+    function preall($alllist)
+    {
         print_r("<pre>");
         print_r($alllist);
         print_r("</pre>");
     }
 
-    function mergeTwoLists($list1, $list2) {
-        $num=[
-            0=>$list1,
-            1=>$list2
+    function mergeTwoLists($list1, $list2)
+    {
+        $num = [
+            0 => $list1,
+            1 => $list2
         ];
-        $alllist=[];
-        for($i=0;$i<2;$i++){
-            foreach($num[$i] as $key=>$value){
-                array_push($alllist,$value);
+        $alllist = [];
+        foreach ($num as $key => $value) {
+            foreach ($value as $key2 => $value2) {
+                array_push($alllist, $value2->value);
             }
         }
         $this->preall($alllist);
-        foreach($alllist as $key=>$value){
-            echo $key;
-            if($value>$alllist[$key+1]){
-                echo "我有近來唷";
-                $temp=$alllist[$key+1];
-                $alllist[$key+1]=$value;
-                $alllist[$key]=$temp;
-                $key=0;
+        while (true) {
+            $count = 0;
+            foreach ($alllist as $key => $value) {
+                if (array_key_last($alllist) != $key) {
+                    if ($value > $alllist[$key + 1]) {
+                        echo "大小轉換開始<br>";
+                        $temp = $alllist[$key + 1];
+                        $alllist[$key + 1] = $value;
+                        $alllist[$key] = $temp;
+                        $count++;
+                    }
+                }
+            }
+            if ($count == 0) {
+                break;
             }
         }
-        return $alllist;
+        $ans=implode(",", $alllist);
+        return $ans;
     }
 }
-$test=new Solution();
-print_r( $test->mergeTwoLists([1,2,4],[1,3,4]))."<br>";
+$test = new Solution();
+print_r($test->mergeTwoLists([1, 2, 4], [1, 3, 4])) . "<br>";
 // print_r($test->mergeTwoLists([],[0]))."<br>";
