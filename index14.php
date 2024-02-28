b056197a92d3d94f923d7ba1f5dbded0d27f714d
Pierre.K <minder309@gmail.com>
2024-02-28 14:11:44 +0800

    第14題方法一完成，現在要寫方法二。

--- index14.php
+++ index14.php
@@ -13,3 +13,32 @@
 // 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
 // 提示：
 // 0 <= x <= 231 - 1
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Integer
+     */
+    function mySqrt($x) {
+        // -----------------方法二-------------------
+        if ($x == 0) {
+            return 0;
+        }
+        
+        
+        
+        
+        
+        // -----------------方法一-------------------
+        // if ($x == 0) {
+        //     return 0;
+        // }
+        // $ans=sqrt($x);
+        // // sqrt() 函数返回一个数的平方根。
+        // return floor($ans);
+        // //floor() 函数向下舍入为最接近的整数。 
+    }
+}
+$solution = new Solution();
+echo $solution->mySqrt(4); //2
+echo $solution->mySqrt(8); //2
