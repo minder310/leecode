629f389d5323eda90bd434d87f16f0d27de487bf
minder310 <minder309@gmail.com>
2024-07-07 17:39:36 +0800

    第16提結束。

--- index16.php
+++ index16.php
@@ -34,20 +34,27 @@ class Solution {
      * @return ListNode
      */
     function deleteDuplicates($head) {
+        // incclassnum是指向head的指標。
         $InClassNum=$head;
+        // 如果inclassnum和inclassnum->next不是null。就運行以下程式。
+        // 因移動指標，所以會減查目前還下一個值是否為空值，如果為空值就return結束遞迴。
         if($InClassNum!=null && $InClassNum->next!=null)
         {
+            // 如果inclassnum的值等於inclassnum->next的值，就會執行以下程式。
             if($InClassNum->val==$InClassNum->next->val)
             {
+                // 會將next->next指向next(等於刪除節點的意思，因為就是把下一個節點，換到next節點，所以刪除了了一個節點的意思)。
                 $InClassNum->next=$InClassNum->next->next;
+                // 回傳改變後的節點結構，並在次呼叫自己檢查是否有重複。
                 $this->deleteDuplicates($InClassNum);
             }
             else
             {
+                // 用遞迴的方式，將數值換到下一個節點，並且導入deleteDuplicates函式，查看是否有重複。
                 $this->deleteDuplicates($InClassNum->next);
             }
         }
-        print_r($InClassNum);
+        
         return $InClassNum;
     }
 }
@@ -57,5 +64,8 @@ $head->next = new ListNode(1);
 $head->next->next = new ListNode(2);
 $Solution = new Solution;
 $Solution->deleteDuplicates($head);
+
+// 這題滿特別的我第一次用遞迴進行解法，其實我一開始還搞不太懂，val->next=val->next->next是什麼意思
+// ，後來才慢慢理解，其實就是把下一個節點的值，換到下下個節點，這樣就等於刪除了一個節點，這樣就可以達到刪除重複的節點。
 
 
